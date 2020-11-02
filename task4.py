d20f93a2529b74b14933ced447c670278632784b
Noor-Shinh <harnoors966@deltalearns.ca>
2020-11-02 11:54:19 -0800

    Changes

--- task4.py
+++ task4.py
@@ -1,4 +1,16 @@
 import tkinter as tk 
 from tkinter import *
 from tkinter import ttk
+window=tk.Tk()
+window.title=("Task 4")
+window.geometry("255x125")
 dogphoto = PhotoImage(file="dog.png")
+label1=tk.Label(window,image=dogphoto)
+label2=tk.Label(window,text="Pochacco!")
+label3=tk.Label(window,text="A cuddly little puppy! This is from the same\n creators who brough you Keropi and Kero Kero",background="#aaffff")
+label1.place(x=60,y=0)
+label2.place(x=125,y=35)
+label3.place(x=0,y=90)
+
+window.mainloop()
+
